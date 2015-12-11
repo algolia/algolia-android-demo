eb4925bba999bb64865e024b18ee57852413f04d
Clément Le Provost <clement.leprovost@algolia.com>
2015-12-11 14:51:44 +0100

    Initial commit: empty project

---
+++ app/src/main/java/algolia/com/demo/moviesearch/ui/MovieSearchActivity.java
@@ -0,0 +1,17 @@
+package algolia.com.demo.moviesearch.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import algolia.com.demo.moviesearch.R;
+
+public class MovieSearchActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_search);
+    }
+}
