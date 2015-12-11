eedd73f1a9f14d0708603ffb74d8f29c472ed59d
Clément Le Provost <clement.leprovost@algolia.com>
2015-12-11 15:47:06 +0100

    Search architecture put in place
    The search does nothing so far.

--- app/src/main/java/algolia/com/demo/moviesearch/ui/MovieSearchActivity.java
+++ app/src/main/java/algolia/com/demo/moviesearch/ui/MovieSearchActivity.java
@@ -23,18 +23,66 @@
 
 package algolia.com.demo.moviesearch.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.support.v7.widget.SearchView;
 
 import algolia.com.demo.moviesearch.R;
 
-public class MovieSearchActivity extends AppCompatActivity
+public class MovieSearchActivity extends AppCompatActivity implements SearchView.OnQueryTextListener
 {
+    private SearchView searchView;
+
+    // Lifecycle
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_search);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_movie_search, menu);
+
+        // Configure search view.
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false);
+        searchView.setOnQueryTextListener(this);
+
+        return true;
+    }
+
+    // Actions
+
+    private void search()
+    {
+        Log.d(this.getClass().getName(), String.format("Search: \"%s\"", searchView.getQuery()));
+    }
+
+    // SearchView.OnQueryTextListener
+
+    @Override
+    public boolean onQueryTextSubmit(String query)
+    {
+        // Nothing to do: the search has already been performed by `onQueryTextChange()`.
+        // We do try to close the keyboard, though.
+        searchView.clearFocus();
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText)
+    {
+        search();
+        return true;
     }
 }
