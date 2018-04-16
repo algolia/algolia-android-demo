3a9f2736fdc8ff286b52815bffa67e588e4a7830
Paul-Louis NECH <pln@algolia.com>
2018-04-16 10:23:33 -0700

    chore(IS): apply migration steps

--- app/src/main/java/algolia/com/demo/moviesearch/ui/ResultsListView.kt
+++ app/src/main/java/algolia/com/demo/moviesearch/ui/ResultsListView.kt
@@ -6,13 +6,13 @@ import android.content.Context
 import android.util.AttributeSet
 import android.widget.ListView
 import com.algolia.instantsearch.model.AlgoliaErrorListener
-import com.algolia.instantsearch.model.AlgoliaResultListener
+import com.algolia.instantsearch.model.AlgoliaResultsListener
 import com.algolia.instantsearch.model.SearchResults
 import com.algolia.search.saas.AlgoliaException
 import com.algolia.search.saas.Query
 
 class ResultsListView(context: Context, attrs: AttributeSet) : ListView(context, attrs),
-        AlgoliaResultListener, AlgoliaErrorListener {
+        AlgoliaResultsListener, AlgoliaErrorListener {
     private val adapter: MovieAdapter = MovieAdapter(context, R.layout.cell_movie)
     private val resultsParser = SearchResultsJsonParser()
 
