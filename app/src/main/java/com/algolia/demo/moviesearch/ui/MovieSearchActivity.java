87d632e207967b12e74305e4817a3b7b815ac86b
Paul-Louis NECH <pln@algolia.com>
2018-09-05 11:19:35 +0200

    wip: Replicate offline bug

--- app/src/main/java/com/algolia/demo/moviesearch/ui/MovieSearchActivity.java
+++ app/src/main/java/com/algolia/demo/moviesearch/ui/MovieSearchActivity.java
@@ -31,6 +31,7 @@ import android.support.annotation.NonNull;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
@@ -40,28 +41,33 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.algolia.demo.moviesearch.R;
+import com.algolia.demo.moviesearch.io.ImageLoaderManager;
+import com.algolia.demo.moviesearch.io.SearchResultsJsonParser;
+import com.algolia.demo.moviesearch.model.HighlightedResult;
+import com.algolia.demo.moviesearch.model.Movie;
 import com.algolia.search.saas.AlgoliaException;
 import com.algolia.search.saas.CompletionHandler;
 import com.algolia.search.saas.MirroredIndex;
+import com.algolia.search.saas.OfflineClient;
+import com.algolia.search.saas.OfflineIndex;
 import com.algolia.search.saas.Query;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.UUID;
 
-import com.algolia.demo.moviesearch.R;
-import com.algolia.demo.moviesearch.io.AlgoliaManager;
-import com.algolia.demo.moviesearch.io.ImageLoaderManager;
-import com.algolia.demo.moviesearch.io.SearchResultsJsonParser;
-import com.algolia.demo.moviesearch.logic.AlgoliaSyncService;
-import com.algolia.demo.moviesearch.model.HighlightedResult;
-import com.algolia.demo.moviesearch.model.Movie;
-
-public class MovieSearchActivity extends AppCompatActivity implements SearchView.OnQueryTextListener, AbsListView.OnScrollListener
-{
+public class MovieSearchActivity extends AppCompatActivity implements SearchView.OnQueryTextListener, AbsListView.OnScrollListener {
     // BL:
     private MirroredIndex index;
     private Query query;
@@ -84,22 +90,77 @@ public class MovieSearchActivity extends AppCompatActivity implements SearchView
 
     private static final int HITS_PER_PAGE = 20;
 
-    /** Number of items before the end of the list past which we start loading more content. */
+    /**
+     * Number of items before the end of the list past which we start loading more content.
+     */
     private static final int LOAD_MORE_THRESHOLD = 5;
 
     // Lifecycle
+    private File createFileFromInputStream(InputStream inputStream, String name) {
+        try {
+            File f = new File(getFilesDir(), name);
+            if (!f.exists()) {
+                f.createNewFile();
+            }
+            OutputStream outputStream = new FileOutputStream(f);
+            byte buffer[] = new byte[1024];
+            int length;
+
+            while ((length = inputStream.read(buffer)) > 0) {
+                outputStream.write(buffer, 0, length);
+            }
+
+            outputStream.close();
+            inputStream.close();
+
+            return f;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_search);
 
-        // Init Algolia.
-        index = AlgoliaManager.getInstance(this).getMoviesIndex();
+        Log.e("PLN", "License:" + getResources().getString(R.string.ALGOLIA_OFFLINE_SDK_LICENSE_KEY));
 
-        // Launch a sync if needed.
-        AlgoliaSyncService.syncIfNeededAndPossible(this);
+        OfflineClient client = new OfflineClient(this, "latency", getResources().getString(R.string.ALGOLIA_API_KEY));
+        client.enableOfflineMode(getResources().getString(R.string.ALGOLIA_OFFLINE_SDK_LICENSE_KEY));
+
+//        Log.e("Files", "Before");
+//        try {
+//            Log.e("Files", "Before getAssets");
+//            String[] assets = getAssets().list("");
+//            Log.e("Files", "After gotAssets, got " + (assets != null ? assets.length : 0));
+//            for (String asset : assets) {
+//                Log.e("Asset name: ", asset);
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        Log.e("Files", "After");
+//
+//        File resourcesDir = new File(getPackageResourcePath() + "/src/app/res/raw/");
+//        Log.e("PLN", "resourcePath: " + getPackageResourcePath());
+//        File[] files = new File(getPackageResourcePath()).listFiles();
+//        Log.d("Files", "Size: "+ files.length);
+//        for (int i = 0; i < files.length; i++)
+//        {
+//            Log.d("Files", "FileName:" + files[i].getName());
+//        }
+        createIndex(client, 100, 0);
+
+        Log.e("PLN", "Done.");
+//        // Init Algolia.
+//        index = AlgoliaManager.getInstance(this).getMoviesIndex();
+//
+//        // Launch a sync if needed.
+//        AlgoliaSyncService.syncIfNeededAndPossible(this);
 
         // Bind UI components.
         moviesListView = (ListView) findViewById(R.id.listview_movies);
@@ -117,10 +178,106 @@ public class MovieSearchActivity extends AppCompatActivity implements SearchView
         displayImageOptions = ImageLoaderManager.getInstance(this).getDisplayImageOptions();
         highlightRenderer = new HighlightRenderer(this);
     }
+//
+//    private class Task extends AsyncTask<Context, Void, Void> {
+//        @Override
+//        protected Void doInBackground(Context... contexts) {
+//            final Context context = contexts[0];
+//
+//            Log.e("PLN", "License:" + context.getResources().getString(R.string.ALGOLIA_OFFLINE_SDK_LICENSE_KEY));
+//
+//            File resourcesDir = new File(context.getPackageResourcePath() + "/src/app/res/raw/");
+//
+//            OfflineClient client = new OfflineClient(context, "latency", context.getResources().getString(R.string.ALGOLIA_API_KEY));
+//            client.enableOfflineMode(context.getResources().getString(R.string.ALGOLIA_OFFLINE_SDK_LICENSE_KEY));
+//
+//            final ArrayList<File> objectFiles = new ArrayList<>();
+//            for (int i = 0; i < 1000; i++) {
+//                objectFiles.add(new File(resourcesDir, "dump.json"));
+//            }
+//            File settingsFile = new File(resourcesDir, "settings.json");
+//
+//
+//            // Create the index.
+//            final OfflineIndex index = client.getOfflineIndex("tests");
+//
+//            // Check that no offline data exists.
+//            Log.e("PLN", "hasOfflineData should be false: " + index.hasOfflineData());
+//
+//            // Build the index.
+//            File[] fileArray = new File[objectFiles.size()];
+//            index.buildFromFiles(settingsFile, objectFiles.toArray(fileArray), new CompletionHandler() {
+//                @Override
+//                public void requestCompleted(JSONObject content, AlgoliaException error) {
+//                    Log.e("PLN", "error should be null: " + error);
+//                    Log.e("PLN", "hasOfflineData should be true: " + index.hasOfflineData());
+//
+//                    // Search.
+//                    Query query1 = new Query().setQuery("peanuts").setFilters("kind:animal");
+//                    index.searchAsync(query1, new CompletionHandler() {
+//                        @Override
+//                        public void requestCompleted(JSONObject jsonObject, AlgoliaException e) {
+//                            Log.e("PLN", "error should be null: " + e);
+//                            Log.e("PLN", "hits: " + jsonObject);
+//                        }
+//                    });
+//                }
+//            });
+//        }
+//    }
+
+
+    public void createIndex(final OfflineClient client, final int limit, final int count) {
+        try {
+            if (limit <= count) {
+                Log.e("PLN", "Reached limit (" + limit + ")");
+                return;
+            }
+            String indexName = UUID.randomUUID().toString();
+            File dumpFile = createFileFromInputStream(getAssets().open("dump.json"), "dump" + indexName + ".json");
+            File settingsFile = createFileFromInputStream(getAssets().open("settings.json"), "settings.json");
+//                Log.e("PLN", "Sfile: " + settingsFile.toString());
+                Log.e("PLN", "Dfile: " + dumpFile.toString());
+            final ArrayList<File> objectFiles = new ArrayList<>();
+            for (int i = 0; i < 1000; i++) {
+                objectFiles.add(dumpFile);
+            }
+
+            // Create the index.
+            final OfflineIndex index = client.getOfflineIndex(indexName);
+
+            // Check that no offline data exists.
+//                Log.e("PLN", "hasOfflineData should be false: " + index.hasOfflineData());
+
+            // Build the index.
+            File[] fileArray = new File[objectFiles.size()];
+            index.buildFromFiles(settingsFile, objectFiles.toArray(fileArray), new CompletionHandler() {
+                @Override
+                public void requestCompleted(JSONObject content, AlgoliaException error) {
+                    //Log.e("PLN", "error should be null: " + error);
+//                        Log.e("PLN", "hasOfflineData should be true: " + index.hasOfflineData());
+                    createIndex(client, limit, count + 1);
+                    Log.e("PLN", "Done creating index " + (count + 1));
+//                        // Search.
+//                        Query query1 = new Query();
+//                        index.searchAsync(query1, new CompletionHandler() {
+//                            @Override
+//                            public void requestCompleted(JSONObject jsonObject, AlgoliaException e) {
+////                                Log.e("PLN", "error should be null: " + e);
+////                                Log.e("PLN", "hits: " + jsonObject);
+//                            }
+//                        });
+                }
+            });
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_movie_search, menu);
 
         // Configure search view.
@@ -147,8 +304,8 @@ public class MovieSearchActivity extends AppCompatActivity implements SearchView
 
     // Actions
 
-    private void search()
-    {
+    private void search() {
+        if (true) return;
         final int currentSearchSeqNo = ++lastSearchedSeqNo;
         query.setQuery(searchView.getQuery().toString());
         lastRequestedPage = 0;
@@ -170,8 +327,7 @@ public class MovieSearchActivity extends AppCompatActivity implements SearchView
                     List<HighlightedResult<Movie>> results = resultsParser.parseResults(content);
                     if (results == null || results.isEmpty()) {
                         endReached = true;
-                    }
-                    else {
+                    } else {
                         moviesListAdapter.clear();
                         moviesListAdapter.addAll(results);
                         moviesListAdapter.notifyDataSetChanged();
@@ -187,8 +343,7 @@ public class MovieSearchActivity extends AppCompatActivity implements SearchView
         });
     }
 
-    private void loadMore()
-    {
+    private void loadMore() {
         Query loadMoreQuery = new Query(query);
         loadMoreQuery.setPage(++lastRequestedPage);
         final int currentSearchSeqNo = lastSearchedSeqNo;
@@ -203,8 +358,7 @@ public class MovieSearchActivity extends AppCompatActivity implements SearchView
                     List<HighlightedResult<Movie>> results = resultsParser.parseResults(content);
                     if (results.isEmpty()) {
                         endReached = true;
-                    }
-                    else {
+                    } else {
                         moviesListAdapter.addAll(results);
                         moviesListAdapter.notifyDataSetChanged();
                         lastDisplayedPage = lastRequestedPage;
@@ -223,17 +377,14 @@ public class MovieSearchActivity extends AppCompatActivity implements SearchView
 
     // Data sources
 
-    private class MovieAdapter extends ArrayAdapter<HighlightedResult<Movie>>
-    {
-        public MovieAdapter(Context context, int resource)
-        {
+    private class MovieAdapter extends ArrayAdapter<HighlightedResult<Movie>> {
+        public MovieAdapter(Context context, int resource) {
             super(context, resource);
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent)
-        {
-            ViewGroup cell = (ViewGroup)convertView;
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewGroup cell = (ViewGroup) convertView;
             if (cell == null) {
                 cell = (ViewGroup) getLayoutInflater().inflate(R.layout.cell_movie, null);
             }
@@ -252,7 +403,7 @@ public class MovieSearchActivity extends AppCompatActivity implements SearchView
         }
 
         @Override
-        public  void addAll(Collection<? extends HighlightedResult<Movie>> items) {
+        public void addAll(Collection<? extends HighlightedResult<Movie>> items) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
                 super.addAll(items);
             } else {
@@ -266,8 +417,7 @@ public class MovieSearchActivity extends AppCompatActivity implements SearchView
     // SearchView.OnQueryTextListener
 
     @Override
-    public boolean onQueryTextSubmit(String query)
-    {
+    public boolean onQueryTextSubmit(String query) {
         // Nothing to do: the search has already been performed by `onQueryTextChange()`.
         // We do try to close the keyboard, though.
         searchView.clearFocus();
@@ -275,8 +425,7 @@ public class MovieSearchActivity extends AppCompatActivity implements SearchView
     }
 
     @Override
-    public boolean onQueryTextChange(String newText)
-    {
+    public boolean onQueryTextChange(String newText) {
         search();
         return true;
     }
@@ -284,14 +433,12 @@ public class MovieSearchActivity extends AppCompatActivity implements SearchView
     // AbsListView.OnScrollListener
 
     @Override
-    public void onScrollStateChanged(AbsListView view, int scrollState)
-    {
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
         // Nothing to do.
     }
 
     @Override
-    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount)
-    {
+    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
         // Abort if list is empty or the end has already been reached.
         if (totalItemCount == 0 || endReached)
             return;
